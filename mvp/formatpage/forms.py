8cc0d392516bb5cf96c86955106589812d64d286
praneethkumarpidugu <pidugu2007@gmail.com>
2015-07-26 18:36:25 -0400

    customized views to render data to home.html

--- mvp/formatpage/forms.py
+++ mvp/formatpage/forms.py
@@ -9,3 +9,18 @@ class PersonalInfoForm(forms.ModelForm):
         model = PersonalInfo
         fields = ['full_name', 'email']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split('@')
+        domain, extension = provider.split('.')
+        if not domain == 'USC':
+            raise forms.ValidationError("Please make sure you use your USC email")
+        if not extension == 'edu':
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        return email
+
+    def clean_full_name(self):
+        full_name = self.cleaned_data.get('full_name')
+
+        return full_name
+
--- mvp/formatpage/views.py
+++ mvp/formatpage/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    title = 'You are not logged in'
+    if request.user.is_authenticated():
+        title = 'My Title %s' %(request.user)
+    context = {
+        "template_title": title,
 
-    return render(request, "home.html", {})
+    }
+
+    return render(request, "home.html", context)
