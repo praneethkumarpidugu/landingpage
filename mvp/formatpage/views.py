fd71c93a4437f39d377614caad963e43d2d8616c
praneethkumarpidugu <pidugu2007@gmail.com>
2015-07-26 22:33:50 -0400

    getting data from forms

--- mvp/formatpage/views.py
+++ mvp/formatpage/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+from .forms import PersonalInfoForm
+
 def home(request):
     title = 'You are not logged in'
-    if request.user.is_authenticated():
-        title = 'My Title %s' %(request.user)
+    form = PersonalInfoForm(request.POST or None)
     context = {
         "template_title": title,
-
+        "form": form
     }
+
+    if form.is_valid():
+        #form.save()
+        instance = form.save(commit=False)
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+        # if not instance.full_name:
+        #     instance.full_name = "Praneeth"
+        instance.save()
+        context = {
+            "template_title": "Thank you"
+        }
 
     return render(request, "home.html", context)
