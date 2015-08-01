2c6064aa3c4e21ff046bfb5550095c183b4ed157
praneethkumarpidugu <pidugu2007@gmail.com>
2015-08-01 18:44:13 -0400

    added the email configuration in views.py and settings.py

--- mvp/formatpage/views.py
+++ mvp/formatpage/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
+
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -31,13 +34,25 @@ def contact(request):
     form = ContactForm(request.POST or None)
 
     if form.is_valid():
-        for key, value in form.cleaned_data.iteritems():
-            print key, value
+        # for key, value in form.cleaned_data.iteritems():
+        #     print key, value
 
-        # email = form.cleaned_data.get("email")
-        # message = form.cleaned_data.get("message")
-        # full_name = form.cleaned_data.get("full_name")
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
         # print email, message, full_name
+        subject = 'Site Contact Form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email]
+        contact_message = "%s: %s via %s"%(
+            form_full_name,
+            form_message,
+            form_email)
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  [to_email],
+                  fail_silently=True)
 
     context = {
         "form": form,
--- mvp/mvp/settings.py
+++ mvp/mvp/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'sampletesting492@gmail.com'
+EMAIL_HOST_PASSWORD = 'poiuytrewq12$#'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
