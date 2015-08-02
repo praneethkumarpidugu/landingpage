9cc395dc78492f1138dba2a52a5f29fef75c4627
praneethkumarpidugu <pidugu2007@gmail.com>
2015-08-01 20:19:11 -0400

    adding debug features in production environment to be False by configuring in urls.py

--- mvp/mvp/urls.py
+++ mvp/mvp/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = [
     # Examples:
@@ -9,3 +12,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
